29ae99b7e2eb0f6cea3b796b30fb0608686c03d1
Andreea <andreea.suciu@emanuel.ro>
2023-11-17 08:23:50 +0200

    initial

---
+++ oop_methods/oop_methods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace oop_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
